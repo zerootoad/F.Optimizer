129a9ba7a4296c719c5f08ca2ffa4a725d51af51
FrancescoTheToad <153208404+FrancescoTheToad@users.noreply.github.com>
2023-12-31 02:16:03 +0100

    Add files via upload

--- python installer.py
+++ python installer.py
@@ -1,6 +1,27 @@
 import os
 import requests
 import glob
+import subprocess
+import time
+
+def next_step(folder):
+    while True:
+        print("Choose an option:")
+        print("1. Install Francesco Optimizer")
+        print("2. Uninstall Francesco Optimizer")
+        print("3. Exit")
+
+        choice = input("Enter the number of your choice: ")
+
+        if choice == "1":
+            install(folder)
+        elif choice == "2":
+            uninstall(folder)
+        elif choice == "3":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please enter a valid number.")
 
 def main():
     print("\n--- Francesco Optimizer Installer ---")
@@ -27,14 +48,14 @@ def find_roblox_folder(pattern):
             return folder
     return None
 
-def next_step(folder):
+def install(folder):
     client_settings_folder = os.path.join(folder, "ClientSettings")
     if not os.path.exists(client_settings_folder):
         os.makedirs(client_settings_folder)
 
     print("Downloading the essential configuration file...\n")
 
-    config_file_url = "https://raw.githubusercontent.com/FrancescoTheToad/F.Optimizer/main/client settings/ClientAppSettings.json"
+    config_file_url = "https://raw.githubusercontent.com/FrancescoTheToad/F.Optimizer/main/modifications/ClientSettings/ClientAppSettings.json"
     config_file_path = os.path.join(client_settings_folder, "ClientAppSettings.json")
     try:
         response = requests.get(config_file_url)
@@ -48,17 +69,70 @@ def next_step(folder):
     
     change_cursors_urls = (
     "https://raw.githubusercontent.com/FrancescoTheToad/F.Optimizer/main/modifications/content/textures/Cursors/KeyboardMouse/ArrowCursor.png",
-    "https://raw.githubusercontent.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png"
+    "https://raw.githubusercontent.com/FrancescoTheToad/F.Optimizer/main/modifications/content/textures/Cursors/KeyboardMouse/ArrowFarCursor.png"
+    )
+
+    change_sounds_urls = (
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/action_falling.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/action_footsteps_plastic.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/action_get_up.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/action_jump.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/action_jump_land.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/action_swim.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/impact_explosion_03.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/impact_water.mp3",
+        "https://github.com/FrancescoTheToad/F.Optimizer/blob/main/modifications/content/sounds/ouch.ogg"
     )
 
     content_folder = os.path.join(folder, "content")
-    for child in content_folder:
-        textures_folder = os.path.join(child, "textures")
-        sounds_folder = os.path.join(child, "sounds")
+    textures_folder = os.path.join(content_folder, "textures")
+    cursors_folder = os.path.join(textures_folder, "Cursors")
+    cursor_folder = os.path.join(cursors_folder, "KeyboardMouse")
+    for texture in os.listdir(cursor_folder):
+        if texture != "IBeamCursor.png":
+            print(f"Replacing: {texture}")
+            os.remove(os.path.join(cursor_folder, texture))
+
+
+    for file_url in change_cursors_urls:
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            filename = os.path.basename(file_url)
+            with open(os.path.join(cursor_folder, filename), "wb") as f:
+                f.write(response.content)
+
+    sounds_folder = os.path.join(content_folder, "sounds")
+    for sound in os.listdir(sounds_folder):
+        if sound != "volume_slider.ogg":
+            print(f"Replacing: {sound}")
+            os.remove(os.path.join(sounds_folder, sound))
+
+    for file_url in change_sounds_urls:
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            filename = os.path.basename(file_url)
+            with open(os.path.join(sounds_folder, filename), "wb") as f:
+                f.write(response.content)
+    
+    print("Installation successful: Francesco Optimizer has been added to your Roblox.\n")
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def uninstall(folder):
+    installer_path = os.path.join(folder, "RobloxPlayerInstaller.exe")
+    if os.path.exists(installer_path):
+        print("Running RobloxInstaller.exe...")
+        subprocess.run([installer_path])
+        found = True
+    else:
+        print("RobloxInstaller.exe not found. Please run it manually.")
+        subprocess.run(["explorer", folder], shell=True)
+        found = False
         
-
-
-    input("Press Enter to exit...")
+    if found:
+        print("Uninstallation successful: Francesco Optimizer has been removed from your Roblox.\n")
+    time.sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == "__main__":
     main()
